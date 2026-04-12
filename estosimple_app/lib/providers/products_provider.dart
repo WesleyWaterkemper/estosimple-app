15c66e27f42c40aed570a5762a0bbc897934d964
Wesley Waterkemper <oobuyuki@gmail.com>
2026-04-12 15:55:46 -0300

    feat: implement ProductsService and ProductsProvider for product management

--- estosimple_app/lib/providers/products_provider.dart
+++ estosimple_app/lib/providers/products_provider.dart
@@ -1,3 +1,88 @@
-class ProductsProvider {
-  
+import 'package:estosimple_app/models/product.dart';
+import 'package:estosimple_app/services/products_service.dart';
+import 'package:flutter/foundation.dart';
+
+class ProductsProvider extends ChangeNotifier{
+  final ProductsService _productsService;
+
+  ProductsProvider(this._productsService);
+ 
+  List<Product> _products = [];
+  List<Product> get products => _products;
+
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+
+  /// Controle de erro
+  String? _error;
+  String? get error => _error;
+
+  Future<void> fetchProducts() async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      _products = await _productsService.getProducts();
+    } catch (e) {
+      _error = e.toString();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> addProduct(Product product) async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      final newProduct = await _productsService.createProduct(product);
+
+      _products.add(newProduct);
+    } catch (e) {
+      _error = e.toString();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> updateProduct(Product product) async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      final updatedProduct = await _productsService.updateProduct(product);
+
+      final index = _products.indexWhere((p) => p.id == updatedProduct.id);
+      if (index != -1) {
+        _products[index] = updatedProduct;
+      }
+    } catch (e) {
+      _error = e.toString();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
+
+  Future<void> removeProduct(int id) async {
+    _isLoading = true;
+    _error = null;
+    notifyListeners();
+
+    try {
+      await _productsService.deleteProduct(id);
+
+      _products.removeWhere((p) => p.id == id);
+    } catch (e) {
+      _error = e.toString();
+    } finally {
+      _isLoading = false;
+      notifyListeners();
+    }
+  }
 }
